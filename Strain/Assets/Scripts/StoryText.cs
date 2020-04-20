2d73c1fc2ebeb80c79d31ae0896d64fca2ac1bc2
Brandon Castillo <brandoncastillo1030@gmail.com>
2020-04-19 22:59:21 -0400

    added random variables, menu, and a few situations

--- Strain/Assets/Scripts/StoryText.cs
+++ Strain/Assets/Scripts/StoryText.cs
@@ -4,20 +4,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class StoryText : MonoBehaviour
 {
     public int health = 100;
     public int stress = 50;
+
+    int stressDecreaseGaming;
+    int stressDecreaseWorkOut;
+    int stressDecreaseRead;
+    int stressDecreaseConsumable;
+
 
     public Text textComponent;
     public State StartingState;
 
     State state;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         state = StartingState;
         textComponent.text = state.GetStateStory();
+        stressDecreaseGaming = UnityEngine.Random.Range(2, 20);
+        stressDecreaseWorkOut = UnityEngine.Random.Range(2, 20);
+        stressDecreaseRead = UnityEngine.Random.Range(2, 20);
+        stressDecreaseConsumable = UnityEngine.Random.Range(2, 20);
     }
 
     // Update is called once per frame
@@ -32,13 +43,33 @@ public class StoryText : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Alpha1)){
             state = nextStates[0];
             health = health + 10;
-            stress = stress - 10;
+            stress = stress - stressDecreaseGaming;
             Debug.Log("Health:" + health);
             Debug.Log("Stress:" + stress);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            state = nextStates[1];
+            state = nextStates[0];
+            health = health + 15;
+            stress = stress - stressDecreaseWorkOut;
+            Debug.Log("Health:" + health);
+            Debug.Log("Stress:" + stress);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[0];
+            health = health + 15;
+            stress = stress - stressDecreaseRead;
+            Debug.Log("Health:" + health);
+            Debug.Log("Stress:" + stress);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            state = nextStates[0];
+            health = health + 15;
+            stress = stress - stressDecreaseConsumable;
+            Debug.Log("Health:" + health);
+            Debug.Log("Stress:" + stress);
         }
         textComponent.text = state.GetStateStory();
     }
